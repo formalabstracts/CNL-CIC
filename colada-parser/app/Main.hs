46507856b555c2ca95dbcf6910403eaa38b6f099
Jesse Michael Han <hyrodi@gmail.com>
2019-08-08 19:47:34 -0400

    Define parser executable

--- colada-parser/app/Main.hs
+++ colada-parser/app/Main.hs
@@ -7,6 +7,21 @@ module Main where
 
 import CNLean.Basic.Basic
 import CNLean.Core
+import qualified Data.Text.IO as TIO
+import System.Environment
+import System.Directory
+import Control.Monad.Trans.State
+import Control.Monad.Trans.State.Lazy
+import Text.Megaparsec
 
 main :: IO ()
-main = test parseProgram "[synonym foo/foos/bar/-s/baz/-s]"
+main = do
+  file <- (head <$> getArgs)
+  txt <- makeAbsolute file >>= TIO.readFile 
+  case (runParser (toParsec parseProgram) "" txt) of
+    Left err -> fail $ show err
+    Right a -> do
+      let output = show a
+      putStrLn output
+      writeFile (file <> "CNL") output
+
